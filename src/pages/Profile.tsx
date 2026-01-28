4e0528f9d37e51b7deaa4101e561952ae8c068cd
Fernando Álisson <118083493+fernandoallisson@users.noreply.github.com>
2026-01-28 16:30:19 -0300

    Updated Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import type { FormEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ArrowLeft } from 'lucide-react'
 
