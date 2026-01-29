b48bcd24a0a9b6ebc9ef616c8592f04d38f5948f
Fernando Álisson <118083493+fernandoallisson@users.noreply.github.com>
2026-01-29 10:21:16 -0300

    Updated Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -186,10 +186,10 @@ const Orders = () => {
                     <div className="flex items-center justify-between">
                       <div>
                         <p className="text-sm font-semibold text-slate-900">
-                          {order.customer.first_name} {order.customer.last_name}  {order.external_id}
+                          {order.customer.first_name} {order.customer.last_name} 
                         </p>
                         <p className="text-xs text-slate-500">
-                         
+                          {order.external_id}
                         </p>
                       </div>
                       <span className="rounded-full bg-slate-100 px-3 py-1 text-xs font-semibold text-slate-600">
